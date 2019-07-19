fe7671e95f4effba7eace08e9bdd9c3eb0af0ade
邹政 <48466557+zouzhn@users.noreply.github.com>
2019-07-19 10:19:40 +0800

    Update worker_test.js

--- Worker多线程使用方法/worker_test.js
+++ Worker多线程使用方法/worker_test.js
@@ -1,5 +1,7 @@
+/*子线程*/
 var data = null;
 onmessage = function(ev){
-	data = ev.data;
-    console.log(data);  // html页面输出{ j: "sjdjd", k: 12 }   注：子线程没有alert函数（因为js中没有alert函数）
+    data = ev.data;
+    console.log(data);  // worker_test.html页面输出{ j: "sjdjd", k: 12 }   注：子线程没有alert函数（因为js中没有alert函数）
+    postMessage({a:'success!', b:'successfully'}); //传回主线程
 };
