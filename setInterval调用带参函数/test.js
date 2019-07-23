4dccf403795de84632dcb5b17c39b9410db63a37
邹政 <48466557+zouzhn@users.noreply.github.com>
2019-07-23 10:34:57 +0800

    Update test.js

--- setInterval调用带参函数/test.js
+++ setInterval调用带参函数/test.js
@@ -4,5 +4,5 @@ function putContent(t) {
 		console.log(a+t);
 	}
 }
-setInterval(putContent(2), 1000); //效果就是：每隔一秒输出6 
+setInterval(putContent(2), 1000); //效果就是：每隔一秒输出6
 console.log('jh'); //程序执行到setInterval的时候，会立即接着执行下面的代码，所以console.log('jh')会先被打印输出，相当于setInterval是一个新的线程
